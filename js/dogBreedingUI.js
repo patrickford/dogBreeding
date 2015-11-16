84df923950f2a055855fecd094d023eab134adb5
Patrick Ford <patrickford@msn.com>
2015-11-15 23:22:50 -0800

    Added table for litter display

--- js/dogBreedingUI.js
+++ js/dogBreedingUI.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
    
   function showLitter(litter) {
-    var dog;
+    var dogRow;
+    $(".dogs").append("<table class='table table-striped'>");
+    $(".dogs").append("<thead><tr><th>Name</th><th>Gender</th><th>Color</th><th>Fur</th><th>Size</th></tr></thead>")
     for (var i = 0; i < litter.length; i++) {
-      dog = "<p> Name: " + litter[i].name + " Gender: " + litter[i].gender + " Color: " + litter[i].color 
-            + " Fur: " + litter[i].fur + " " + " Size: " + litter[i].size + "</p>";
-            + " "
-      $(".dogs").append(dog);
+      dogRow = "<tr>";
+      dogRow += "<td>" + litter[i].name + "</td>";
+      dogRow += "<td>" + litter[i].gender + "</td>";
+      dogRow += "<td>" + litter[i].color + "</td>";
+      dogRow += "<td>" + litter[i].fur + "</td>"; 
+      dogRow += "<td>" + litter[i].size + "</td>";
+      dogRow += "</tr";
+      $(".dogs").append(dogRow);
     }
+    $(".dogs").append("</table>"); 
   }
 
   showLitter(litter);
